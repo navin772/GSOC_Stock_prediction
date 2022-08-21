454cc11e9f6f270419488168e194380f406c6e90
Navin Chandra <navinchandra772@gmail.com>
2022-08-21 21:40:31 +0530

    Added try except

--- fin_dashboard/stock_recomendation.py
+++ fin_dashboard/stock_recomendation.py
@@ -14,13 +14,18 @@ def analyst_recommendation(ticker):
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
     }
     url = lhs_url + ticker + rhs_url
-    r = requests.get(url, headers=headers)
-    result = r.json()["quoteSummary"]["result"][0]
+    try:
 
-    reco_1 = result["financialData"]["targetMeanPrice"]["fmt"].replace(",", "")
-    # reco_2 = result["financialData"]["targetHighPrice"]["fmt"].replace(",", "")
+        r = requests.get(url, headers=headers)
+        result = r.json()["quoteSummary"]["result"][0]
 
-    reco_1 = float(reco_1)
-    # reco_2 = float(reco_2)
+        reco_1 = result["financialData"]["targetMeanPrice"]["fmt"].replace(",", "")
+        # reco_2 = result["financialData"]["targetHighPrice"]["fmt"].replace(",", "")
 
-    return (reco_1)
+        reco_1 = float(reco_1)
+        # reco_2 = float(reco_2)
+
+        return (reco_1)
+
+    except KeyError:
+        return ("This ticker does not have Analyst Recommendations")
